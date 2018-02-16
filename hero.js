5e164fe3bde1da74bcfb3247ac2b31d78e94cc69
Max Robson <maxwell.robson@gmail.com>
2018-02-16 12:42:32 +0000

    init... again...

---
+++ hero.js
@@ -0,0 +1,9 @@
+const Hero = function (name, favoriteFood) {
+  this.name = name;
+  this.health = 100;
+  this.favoriteFood = favoriteFood;
+  this.talk = function (){
+    return `Good day, i am ${this.name}`
+  };
+  this.heroTasks = [];
+}
---
+++ specs/hero_spec.js
@@ -0,0 +1,17 @@
+const assert = require ('assert')
+const hero = require ('../hero.js')
+
+describe('Hero' function() {
+
+  let hero;
+
+  beforeEach(function(){
+    hero = new Hero("Sir. Lengthy Pork-sword", "Sausge")
+  });
+
+  it('should have a name', function(){
+    assert.strictEqual(hero.name, "Sir. Lengthy Pork-sword")
+  });
+
+  // it('should have a favoriteFood')
+});
