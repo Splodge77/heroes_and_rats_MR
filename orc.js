97e356ba128e2ccec8ab5d8f5221672e87aec886
Max Robson <maxwell.robson@gmail.com>
2018-02-17 10:13:36 +0000

    orc has a weapon and base damage

--- orc.js
+++ orc.js
@@ -1,6 +1,8 @@
-const Orc = function(name){
+const Orc = function(name, weapon){
   this.name = name;
   this.health = 150;
+  this.damage = 30;
+  this.weapon = weapon;
 }
 
 module.exports = Orc;
--- specs/orc_spec.js
+++ specs/orc_spec.js
@@ -6,7 +6,7 @@ describe("Orc Test", function(){
   let orc;
 
   beforeEach(function(){
-    orc1 = new Orc("Lok-tar")
+    orc1 = new Orc("Lok-tar", "Axe")
   });
 
   it('should have a name', function(){
@@ -15,5 +15,13 @@ describe("Orc Test", function(){
 
   it('should have a base health', function(){
     assert.deepStrictEqual(orc1.health, 150)
+  });
+
+  it('should have a base damage', function(){
+    assert.deepStrictEqual(orc1.damage, 30)
+  });
+
+  it('should start with a weapon', function(){
+    assert.deepStrictEqual(orc1.weapon, "Axe")
   });
 });
