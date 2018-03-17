fadd5ba65e5fc385e26bc47e7343379c96b19a7d
Max Robson <maxwell.robson@gmail.com>
2018-03-17 18:54:42 +0000

    set up canvas with load event listener in app

---
+++ new_work/client/src/app.js
@@ -0,0 +1,10 @@
+
+
+window.addEventListener('load', () => {
+  const canvas = document.getElementById('main-canvas');
+  const context = canvas.getContext('2d');
+
+  canvas.addEventListener('click', (event) => {
+
+  })
+});
